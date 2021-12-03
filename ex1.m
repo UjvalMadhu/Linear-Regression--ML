90a81b5b542a1de5c8f2dc77dc6fb746adb3c9cc
UjvalMadhu <93603056+UjvalMadhu@users.noreply.github.com>
2021-12-03 20:05:04 +0530

    Update ex1.m

--- ex1.m
+++ ex1.m
@@ -1,13 +1,6 @@
 %% Machine Learning Online Class - Exercise 1: Linear Regression
 
-%  Instructions
-%  ------------
-%
-%  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions
-%  in this exericse:
-%
-%     warmUpExercise.m
+% The Different files asscociated with the exercise are: 
 %     plotData.m
 %     gradientDescent.m
 %     computeCost.m
@@ -16,8 +9,7 @@
 %     featureNormalize.m
 %     normalEqn.m
 %
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
+
 %
 % x refers to the population size in 10,000s
 % y refers to the profit in $10,000s
@@ -26,35 +18,24 @@
 %% Initialization
 clear ; close all; clc
 
-%% ==================== Part 1: Basic Function ====================
-% Complete warmUpExercise.m
-fprintf('Running warmUpExercise ... \n');
-fprintf('5x5 Identity Matrix: \n');
-warmUpExercise()
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
-
-%% ======================= Part 2: Plotting =======================
+%% ======================= Part 1: Plotting =======================
 fprintf('Plotting Data ...\n')
 data = load('ex1data1.txt');
 X = data(:, 1); y = data(:, 2);
 m = length(y); % number of training examples
 
 % Plot Data
-% Note: You have to complete the code in plotData.m
-plotData(X, y);
+plotData(X, y); % Check the code in plotData
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
-%% =================== Part 3: Cost and Gradient descent ===================
+%% =================== Part 2: Cost and Gradient descent ===================
 
 X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
 theta = zeros(2, 1); % initialize fitting parameters
 
-% Some gradient descent settings
+% Initialising gradient descent settings
 iterations = 1500;
 alpha = 0.01;
 
@@ -62,7 +43,6 @@ fprintf('\nTesting the cost function ...\n')
 % compute and display initial cost
 J = computeCost(X, y, theta);
 fprintf('With theta = [0 ; 0]\nCost computed = %f\n', J);
-fprintf('Expected cost value (approx) 32.07\n');
 
 % further testing of the cost function
 J = computeCost(X, y, [-1 ; 2]);
@@ -79,8 +59,6 @@ theta = gradientDescent(X, y, theta, alpha, iterations);
 % print theta to screen
 fprintf('Theta found by gradient descent:\n');
 fprintf('%f\n', theta);
-fprintf('Expected theta values (approx)\n');
-fprintf(' -3.6303\n  1.1664\n\n');
 
 % Plot the linear fit
 hold on; % keep previous plot visible
